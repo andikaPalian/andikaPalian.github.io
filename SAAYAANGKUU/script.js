7109e6b0f71b6ca9afe2eb345f488a5ced40d008
andikaPalian <152042381+andikaPalian@users.noreply.github.com>
2024-03-01 09:33:00 +0800

    Add files via upload

---
+++ SAAYAANGKUU/script.js
@@ -0,0 +1,15 @@
+const text = document.querySelector(".sec-text");
+
+const textLoad = () => {
+    setTimeout(() => {
+        text.textContent = "Cintakuuuu❤️";
+    }, 0);
+    setTimeout(() => {
+        text.textContent = "Cantikkuuu💕";
+    }, 4000);
+    setTimeout(() => {
+        text.textContent = "I LOVEE YOUU SAYANGG 🤍";
+    }, 8000);
+}
+
+textLoad();
