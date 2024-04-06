70dd43e10699464cf2dc15b79781c4422f474209
andikaPalian <152042381+andikaPalian@users.noreply.github.com>
2024-04-06 11:27:49 +0800

    Add files via upload

---
+++ tersayang/script.js
@@ -0,0 +1,170 @@
+$(document).ready(function () {
+    /*
+     * Main variables
+     */
+    var content = [{
+        title: "Rezky Amalia Tersayangg❤️",
+        desc: "baca na sayang, pencetki tombol nextnya yang tanda panah di bawah itu kalau mau lanjut mu baca"
+    }, {
+        title: "Terima Kasih Sayangkuu",
+        desc: "terima kasih Liaa sudah mau terima dan sayang sama Dika walaupun Dika banyak kurangnya dan tidak ada yang bisa di banggakan dari Dika, tapi Liaa mau terima Dika jadi pasangannya Liaa, mau sayang sama Dika,jujur tidak nyangkaka waktu itu mu terimaka jadi pacarmu Liaa karena ku pikir siapa saya berharap bisa sama cewek hebat dan cantik kayak Liaa tapi ternyata mauko terima ka apa adanya mau sayang sama Dika, Terima Kasih Banyak Liaa Sayangkuuu"
+    }, {
+       title: "Bersyukur Bisa Dengan Liaa",
+       desc: "jujur Liaa sangan bersyukurka dengan kehadiranmu di hidupku Liaa, banyak yang berubah dari hidupku semenjak kenal dan bersama-sama dengan Liaa, hariku lebih bahagia, selalu exited setiap hari, jadi lebih semangatka, lebih berwarna hariku, dan masih banyak lagi yang kusyukuri semenjak kenal dan pacaran sama Liaa karena dipertemukan dengan wanita baik dan cantik seperti Liaa"
+    }, {
+        title: "Bahagia Bisa Bersama Dengan Liaa",
+        desc: "bahagia sekalika Liaa semenjak kenal dan pacaran sama Liaa, dulu yang hidupku kubenci kurasa suram membosankan tapi semenjak kehadiran Liaa jadi berubah sayangg jadi lebih bahagiaka semangatka setiap hari intinya bahagia sekalika sama Liaa, jujur sayangg setiap bangun pagika biasa sudah berdoa sama Tuhan, baru cuci muka habis itu ambilma hp ku kasih nyala pasti langsung cek wa mu kalau tidak ada atau belum aktif pasti itu ku kasih selamat pagi, atau sebelum tidur ku ucapkan Good Night dan bahagiaka lakukan itu, semoga juga tidak bosan jako kalau ku ucapkan selamat malam dan pagi terus sayang"
+    }, {
+        title: "Sayang Dan Cinta Sekalika Sama Liaa",
+        desc: "sayanggg dan cintaaaa sekalika sama Liaa tidak tau bagaimana caranya ungkapkanki rasa sayang dan cintaku ke Liaa karena tidak bisa diukur dengan apa pun rasa sayang dan cintaku ke Liaa, tidak akan pernahka bosan dan capek untuk sayangg dan cinta terus ke Liaaa, tidak akan pernah berkurang rasa sayang dan cintaku ke Liaa malahan tambah hari malah bertambah sayang dan cinta, dan tidak akan bosanka bilang kalau ku sayanggg dan cintaa sekaliko Liaaa"
+ }, {
+    title: "Minta Maaf ka Sayang",
+    desc: "mintaa maaaffka na sayang kalau selama ini pacaranki tidak pernahpa kasihko gift atau mungkin tidak adapi efortku ke kau sayangg, mintaa maaffka kalau kadang sukaka ovt atau berpikiran berlebihan dan selalu muncul pertanyaanku yang buatko capek jawabki atau pikirki sayangg, mintaa maaffka juga kalau ada perbuatan perilaku atau sikapku yang suka buatko marah ilfeel atau risih sayanggg, dan mintaa maaffka kalau mungkin ada beberapa kondisi bertanyaka atau pertanyaanku yang buatko berfikir tidak ku percayako sayangg, harus ko tau sayang sangat ku percayako 100% ku percayako, minta maaafka kalau biasa ceritaki atau bicaraki itu kaku ka dan membosankan ka jujur memang agak kakuka saya tapi itu mungkin kalau sering-sering maki bicara atau ketemu kayak mungkin keluarmi gilaku segala macam hahaha"
+ }, {
+    title: "Permintaanku Sayangg",
+    desc: "kasihka kabar seperti lagi apa lagi dimana pergi kemana sama siapa naik apa biar tidak khawatirka sayangg, jangan friendly juga ke cowok sayang mu taumi toh gampangka cemburu jujur dekat dan sampai kadang bercanda bahagia sekaliko sama cowok lain cemburu sekalika ka tidak begituko kalau di saya, terbukako dengan saya na sayangg kalau ada dekatiko atau mantanmu chat kasih tauka sayangg, setia na ke saya sayangg, tulus juga ke saya sayanggg, sama ini maaf na bukanja bermaksud jangan selingkuh na sayangg kalau ada salahku atau tidak mu suka dari saya bilang na bakalan ku ubah demi kau sayangkuu jadi janganna sampai ada orang ketiga dihubunganta, jadi diri sendiri juga sayangg mauko pelukka boleh mauko pegang gandeng tanganku boleh ku sukaji karena ku rasa kalau begituko mu sayangg sekalika jadi laukakanmi saja apa mu suka atau mu maui kalau jalan atau sama saya sayanggg jujur kalau begitu kurasa di sayangg sekalika, semoga juga tidak akan berkurang atau berubah rasa sayangg dan cintamu ke saya sayanggg, dan juga tegurka sayang kalau ada salahku ku buat, intinya saya jaga kepercayaanmu kau juga jaga kepercayaanku saya jaga hati dan perasaanmu kau juga jaga hati dan perasaanku minta maafka kalau ada beberapa permintaanku yang mungkin terlihat egois sayanggg dan minta maaf kalau ada yang mu salah pahami dari permintaanku bisaji mu bilang di wa kalau ada mu salah pahami sayangg"
+ }, {
+    title: "Jangan Insecure Liaa Sayangkuu",
+    desc: "jangan suka bandingkan dirimu dengan cewek lain ya sayangg tidak pedulija juga kalau mu bilang banyak cewek lebih cantik lebih ini karena tetapji kalau dimataku kau yang lebih cantik dan lebih lebih dari mereka sayangg kau itu sempurna di mataku sayangg, itu yang mu pikir tidak pintarko pintar jako sayangg tidak ada orang tidak pintar dan banggaka sama kau sayanggg dengan prestasi mu dengan kecantikanmu dengan sikapmu perhatianmu dan kasih sayangmu ke saya Liaa sayangkuuu yang ku perlukan itu kasih sayang cintamu tulusmu setiamu ke saya ituji saja sayang soal cantik dan lain lain sudah lebih cantik dan lebih-lebih mako sayang dibandingkan cewek lain, jadi jangan insecure atau bandingkan dirimu dengan cewek lain ya sayang Liaa sudah lebih dari cukupmi untuk Dika"
+ }, {
+    title: "Semangat Dan Kuat ya Sayangkuuu",
+    desc: "walaupun banyak masalah dan beban pikiranmu sayang harus tetap kuat dan semangat sayangg, bisaji juga mu cerita kalau ada masalahmu ada beban pikiranmu atau mauko cerita hari-harimu ke saya sayangkuuu siapja jadi pendengar buat kau sayangg, selaluja ada kalau butuhko jadi jangan anggap dirimu sendirina adaja selalu sayangg, janganmi pikir takut mu repotkanka atau apa ku sukaji sayang kalau kayak mu repotkanka atau minta tolongko ke saya merasa di anggapka kalau begituko jadi kalau ada apa apamu tanya saja saya na sayangggg, ingat harus selalu semangat dan kuat ya sayanggg karena Liaa itu wanita yang hebat dan kuat, pasti setiap masalah akan selesaiji sayang walaupun mungkin berat dan sulit, jadi harus tetap semangat dan kuat dan berdoa sama Tuhan yaaa sayangggkuuu"
+ }, {
+    title: "Doa dan Harapanku",
+    desc: "selaluka berdoa biar Tuhan kuatkanki hadapi semua masalah dan tantangan, supaya Tuhan kuatkan hubunganta sampai jenjang pernikahan dan sampai menua bersama, bahkan doaku itu supaya Tuhan tutup dan kunci itu pintu hatimu untuk pria lain biar hatimu cuman untuk saya dan berdoaka supaya tulus dan setiaku sama saya dan saya jugua begitu, berdoaka juga sama Tuhan supaya tidak akanka buatko kecewa dan sakit hati karena perbuatanku, berdoaka supaya rasa sayangg dan cinta ta tidak akan ada habis dan kurangnya untuk selamanya biar selamanyaki saling sayangg dan cintaaa, dan berdoaka supaya ini hubungan bisa ke jenjang yang lebih serius itu sampai menikahki menuaki bersama intinya sayang dan cintaki selama-lamanya"
+ }, {
+    title: "Hehehehe",
+    desc: "jujur na sayanggg jawab di wa ini heheh, sebenarnya mauko sama saya apa yang mu lihat dari saya sampai yakinko dengan saya apa yang spesial dari saya sayangg? Di jawab ya sayangg HEhehehe"
+ }, {
+    title: "Dari Pacarmu Andika Palian",
+    desc: "Wopyuuuuuuuuu Liaaa sayanggkuuu, mungkin itu dulu yang ku sampaika kalau ada lagi nanti ku tanyakan ke kau sayanggg, dan kalau ada mu salah pahami tanyaka na sama mintaa maafka kalau ada yang mu salah pahami sayanggg"
+ }, {
+    title: "LOVEEE YOUUUU FOREEEEVERRR LIAAAA SAYAANGGGGKUUUU CINTAAKUUU CANTIKUUU DUNIAAKUUU MANISSKUUU❤️",
+    desc: "tetap sayang dan cinta sama saya yaa sayaanggggggg semoga tidak berkurang atau berubah rasa sayanggmu ke saya Liaaa sayangkuuuu"
+ }];
+    var currentPage = 0;
+    //generate content
+    for (var i = 0; i < content.length; i++) {
+        //split content letters to array
+        for (var obj in content[i]) {
+            //if string
+            if (typeof content[i][obj] === "string") {
+                content[i][obj] = content[i][obj].split("");
+                continue;
+            }
+            //if array (grouped text)
+            else if (typeof content[i][obj] === "object") {
+                var toPush = [];
+                for (var j = 0; j < content[i][obj].length; j++) {
+                    for (var k = 0; k < content[i][obj][j].length; k++) {
+                        toPush.push(content[i][obj][j][k]);
+                    }
+                }
+                content[i][obj] = toPush;
+            }
+        }
+        //set text to 
+        $("#segments").append("<div class=\"letters-wrap mutable\"><div class=\"soup-title\"></div><div class=\"soup-desc\"></div></div>");
+        setText();
+        //clone to data
+        $("#segments").append("<div class=\"letters-wrap position-data\"><div class=\"soup-title\"></div><div class=\"soup-desc\"></div></div>");
+        setText();
+    }
+    //initial arrangement
+    arrangeCurrentPage();
+    scrambleOthers();
+    /*
+     * Event handlers
+     */
+    $(window).resize(function () {
+        arrangeCurrentPage();
+        scrambleOthers();
+    });
+    $("#soup-prev").hide();
+    $("#soup-prev").click(function () {
+        $("#soup-next").show();
+        currentPage--;
+        if (currentPage === 0) {
+            $("#soup-prev").hide();
+        }
+        arrangeCurrentPage();
+        scrambleOthers();
+    });
+    $("#soup-next").click(function () {
+        $("#soup-prev").show();
+        currentPage++;
+        if (currentPage === content.length - 1) {
+            $("#soup-next").hide();
+        }
+        arrangeCurrentPage();
+        scrambleOthers();
+    });
+    /*
+     * Functions
+     */
+    function arrangeCurrentPage() {
+        for (var i = 0; i < content[currentPage].title.length; i++) {
+            $(".mutable:eq(" + currentPage + ") > .soup-title > .letter").eq(i).css({
+                left: $(".position-data:eq(" + currentPage + ") > .soup-title > .letter").eq(i).offset().left + "px",
+                top: $(".position-data:eq(" + currentPage + ") > .soup-title > .letter").eq(i).offset().top + "px",
+                color: "#111",
+                zIndex: 9001
+            });
+        }
+        for (var i = 0; i < content[currentPage].desc.length; i++) {
+            $(".mutable:eq(" + currentPage + ") > .soup-desc > .letter").eq(i).css({
+                left: $(".position-data:eq(" + currentPage + ") > .soup-desc > .letter").eq(i).offset().left + "px",
+                top: $(".position-data:eq(" + currentPage + ") > .soup-desc > .letter").eq(i).offset().top + "px",
+                color: "#111",
+                zIndex: 9001
+            });
+        }
+    }
+
+    function setText() {
+        var j;
+        for (j = 0; j < content[i].title.length; j++) {
+            $(".soup-title").last().append("<span class=\"letter\">" + content[i].title[j] + "</span>");
+        }
+        for (j = 0; j < content[i].desc.length; j++) {
+            $(".soup-desc").last().append("<span class=\"letter\">" + content[i].desc[j] + "</span>");
+        }
+    }
+
+    function scrambleOthers() {
+        for (var i = 0; i < content.length; i++) {
+            //don't scramble currentPage
+            if (currentPage === i)
+                continue;
+            var parts = [
+                ["title", ".soup-title"],
+                ["desc", ".soup-desc"]
+            ];
+            //apply to .title h1s and .desc ps
+            for (var j = 0; j < parts.length; j++) {
+                for (var k = 0; k < content[i][parts[j][0]].length; k++) {
+                    //define random position on screen
+                    var randLeft = Math.floor(Math.random() * $(window).width());
+                    var randTop = Math.floor(Math.random() * $(window).height());
+                    //defining boundaries
+                    var offset = $(".position-data").eq(currentPage).offset();
+                    var bounds = {
+                        left: offset.left,
+                        top: offset.top,
+                        right: $(window).width() - offset.left,
+                        bottom: $(window).height() - offset.top
+                    };
+                    var middleX = bounds.left + $(".position-data").eq(currentPage).width() / 2;
+                    var middleY = bounds.top + $(".position-data").eq(currentPage).height() / 2;
+                    //finally, apply all the scrambles
+                    $(".mutable:eq(" + i + ") > " + parts[j][1] + " > .letter").eq(k).css({
+                        left: randLeft,
+                        top: randTop,
+                        color: "#DDD",
+                        zIndex: "initial"
+                    });
+                }
+            }
+        }
+    }
+});
